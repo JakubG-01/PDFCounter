e5ce1097efcc4619941ed6b22db6d64ea6b3dd3a
qb3k <jacob.gubar@gmail.com>
2025-08-28 13:36:30 +0200

    fix: added warning message if folder was not selected

--- src/PDFCounter/GUI/start_analyzing.py
+++ src/PDFCounter/GUI/start_analyzing.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
 from analyzers import PDFBatchAnalyzer
 
@@ -24,16 +25,21 @@ class StartAnalyzing(ttk.Frame):
         analyze_button.pack(side="left")
 
     def changeProgress(self):
+        self.progress_bar['value'] = 0
         self.output_frame.results_box.config(state=tk.NORMAL)
         self.output_frame.results_box.delete("1.0", "end")
-        self.progress_bar['value'] = 100
-        self.analyzer = PDFBatchAnalyzer(folder_path=self.file_select_frame.selected_folder,
-                                         price_bw=self.config_data["PRICE_FOR_BW"],
-                                         price_color=self.config_data["PRICE_FOR_COLOR"],
-                                         pixel_to_mm=self.config_data["PIXEL_TO_MM"],
-                                         a4_format=self.config_data["A4_WIDTH_MM"] *
-                                         self.config_data["A4_HEIGHT_MM"]
-                                         )
-        result = self.analyzer.analyze_all()
-        self.output_frame.results_box.insert("end", result)
+        if self.file_select_frame.selected_folder == "":
+            messagebox.showwarning(
+                "Warning", "Please select a folder before proceeding")
+        else:
+            self.progress_bar['value'] = 100
+            self.analyzer = PDFBatchAnalyzer(folder_path=self.file_select_frame.selected_folder,
+                                             price_bw=self.config_data["PRICE_FOR_BW"],
+                                             price_color=self.config_data["PRICE_FOR_COLOR"],
+                                             pixel_to_mm=self.config_data["PIXEL_TO_MM"],
+                                             a4_format=self.config_data["A4_WIDTH_MM"] *
+                                             self.config_data["A4_HEIGHT_MM"]
+                                             )
+            result = self.analyzer.analyze_all()
+            self.output_frame.results_box.insert("end", result)
         self.output_frame.results_box.config(state=tk.DISABLED)
