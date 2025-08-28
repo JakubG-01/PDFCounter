c5ac9107ff0a2efcc6243c5d355e048aade2673c
qb3k <jacob.gubar@gmail.com>
2025-08-28 17:30:37 +0200

    refactor: changed old analyzers to generators, separated to smaller functions

--- src/PDFCounter/analyzers/batch_analyzer.py
+++ src/PDFCounter/analyzers/batch_analyzer.py
@@ -1,5 +1,5 @@
-import os
 from .file_analyzer import PDFFileAnalyzer
+import os
 
 
 class PDFBatchAnalyzer:
@@ -15,29 +15,44 @@ class PDFBatchAnalyzer:
         self.total_blank = 0
         self.total_cost = 0.0
 
+    def analyze_file(self, filepath):
+        file_analyzer = PDFFileAnalyzer(
+            filepath=filepath,
+            price_bw=self.price_bw,
+            price_color=self.price_color,
+            pixel_to_mm=self.pixel_to_mm,
+            a4_format=self.a4_format
+        )
+        msg = file_analyzer.analyze()
+        bw, color, cost, blank = file_analyzer.get_summary()
+
+        return msg, bw, color, blank, cost
+
     def analyze_all(self):
-        self.result = ""
+        self.total_bw = 0
+        self.total_color = 0
+        self.total_blank = 0
+        self.total_cost = 0.0
+
         for filename in os.listdir(self.folder_path):
             filepath = os.path.join(self.folder_path, filename)
+
             if os.path.isfile(filepath) and filename.lower().endswith(".pdf"):
-                file_analyzer = PDFFileAnalyzer(
-                    filepath=filepath,
-                    price_bw=self.price_bw,
-                    price_color=self.price_color,
-                    pixel_to_mm=self.pixel_to_mm,
-                    a4_format=self.a4_format
-                )
-                self.result += file_analyzer.analyze() + "\n"
-                bw, color, cost, blank = file_analyzer.get_summary()
+                msg, bw, color, blank, cost = self.analyze_file(filepath)
                 self.total_bw += bw
                 self.total_color += color
-                self.total_cost += cost
                 self.total_blank += blank
-                self.result += "\n"
+                self.total_cost += cost
 
-        return self.print_summary()
+                yield (filename, msg, bw, color, blank, cost)
+
+        yield self.print_summary()
 
     def print_summary(self):
-        summary_text = self.result + \
-            f"📊 SUMMARY: \nBW PAGES: {self.total_bw} \nCOLOR PAGES: {self.total_color} \nBLANK PAGES: {self.total_blank} \nTOTAL COST: {round(self.total_cost, 2)} PLN"
-        return summary_text
+        return (
+            f"📊 SUMMARY:\n"
+            f"  BW PAGES: {self.total_bw}\n"
+            f"  COLOR PAGES: {self.total_color}\n"
+            f"  BLANK PAGES: {self.total_blank}\n"
+            f"  TOTAL COST: {round(self.total_cost, 2)} zł\n"
+        )
