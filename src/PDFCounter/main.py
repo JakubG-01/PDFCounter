d84d667af03a139e63dfe2ad26655b1c55e086c9
qb3k <jacob.gubar@gmail.com>
2025-09-06 12:07:13 +0200

    refactor: deleted unused constant

--- src/PDFCounter/main.py
+++ src/PDFCounter/main.py
@@ -9,7 +9,5 @@ config_path = os.path.join(BASE_DIR, "config", "config.json")
 with open(config_path, "r") as f:
     config = json.load(f)
 
-A4_FORMAT = 62370
-
 if __name__ == "__main__":
     app = PDFCounterApp(config)
