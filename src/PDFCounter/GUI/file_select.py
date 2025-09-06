96b2e23954a3f89db9380082245f0c7d602d5e10
qb3k <jacob.gubar@gmail.com>
2025-09-06 13:06:39 +0200

    fix: added initialization of selected_folder, removed f strings

--- src/PDFCounter/GUI/file_select.py
+++ src/PDFCounter/GUI/file_select.py
@@ -10,12 +10,14 @@ class FileSelect(ttk.Frame):
             side="left", padx=(0, 10))
         self.pack(side='top', fill='x')
 
+        self.selected_folder = ""
+
         self.create_widgets()
 
     def create_widgets(self):
         button = ttk.Button(self, text="Browse")
         self.chosen_directory = ttk.Label(
-            self, text=f"Folder was not selected")
+            self, text="Folder was not selected")
         self.chosen_directory.pack(side="right")
         button.pack(side="left")
         button.config(command=self.chooseDirectory)
@@ -23,7 +25,7 @@ class FileSelect(ttk.Frame):
     def chooseDirectory(self):
         self.selected_folder = filedialog.askdirectory()
         if self.selected_folder == "":
-            self.chosen_directory.config(text=f"Folder was not selected")
+            self.chosen_directory.config(text="Folder was not selected")
         else:
             self.chosen_directory.config(
                 text=f"Selected folder: {self.selected_folder}")
